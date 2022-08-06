49ac81897d18bba28af3ee860f4ebfe5de6b0d8c
B.Orgilbat <zettry1@gmail.com>
2022-08-06 09:48:13 -0500

    chorE: code format

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,59 +1,57 @@
-const Category = require("../models/categoryModel")
+const Category = require("../models/categoryModel");
 
 async function addCategory(req, res, next) {
-    try {
-        const results = await Category.create(req.body);
-        res.json(results);
-    } catch (error) {
-        next(error);
-    }
-
+  try {
+    const results = await Category.create(req.body);
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function getCategories(req, res, next) {
-    try {
-        const results = await Category.find({});
-        res.json(results);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const results = await Category.find({});
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function getCategoryById(req, res, next) {
-    try {
-        const results = await Category.findById(req.params.categ_id);
-        res.json(results)
-    } catch (error) {
-        next(error);
-    }
-
+  try {
+    const results = await Category.findById(req.params.categ_id);
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function deleteCategoryById(req, res, next) {
-    try {
-        const results = await Category.deleteOne({ _id: req.params.categ_id });
-        res.json(results);
-    } catch (error) {
-        next(error);
-    }
-
+  try {
+    const results = await Category.deleteOne({ _id: req.params.categ_id });
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function updateCategoryById(req, res, next) {
-    try {
-        console.log(req.body)
-        const results = await Category.updateOne({ _id: req.params.categ_id },
-            { $set: { ...req.body } });
-        res.json(results);
-    } catch (error) {
-        next(error);
-    }
-
+  try {
+    console.log(req.body);
+    const results = await Category.updateOne(
+      { _id: req.params.categ_id },
+      { $set: { ...req.body } }
+    );
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
 }
 module.exports = {
-    addCategory,
-    getCategories,
-    getCategoryById,
-    deleteCategoryById,
-    updateCategoryById,
-}
+  addCategory,
+  getCategories,
+  getCategoryById,
+  deleteCategoryById,
+  updateCategoryById,
+};
