ec9ab5aade2ec25640360fa76e051137e130d338
B.Orgilbat <zettry1@gmail.com>
2022-08-09 16:45:43 -0500

    cat list update

--- client/src/app/component/add-category/add-category.component.ts
+++ client/src/app/component/add-category/add-category.component.ts
@@ -7,38 +7,54 @@ import { CategoriesService } from 'src/app/service/categories.service';
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.scss']
+  styleUrls: ['./add-category.component.scss'],
 })
 export class AddCategoryComponent implements OnInit {
-  form!:FormGroup;
+  form!: FormGroup;
   category!: Category;
   imageData!: string;
+  categories: Array<Category> = [];
   private router!: Router;
 
-  constructor( private categoryService : CategoriesService) { }
-
+  constructor(
+    private categoryService: CategoriesService,
+    private categoriesService: CategoriesService
+  ) {}
+  fetchCategories() {
+    console.log('fetch data');
+    this.categoriesService
+      .getCategories()
+      .subscribe((categs: Array<Category>) => {
+        console.log('categs,categs', categs);
+        this.categories = categs;
+      });
+  }
   ngOnInit(): void {
     this.form = new FormGroup({
-      name : new FormControl(null),
+      name: new FormControl(null),
       image: new FormControl(null),
-    })
+    });
+    this.fetchCategories();
   }
 
   saveData() {
     console.log('save category');
-    this.categoryService.addNewCateegory(this.form.value.name, this.form.value.image);
+    this.categoryService.addNewCateegory(
+      this.form.value.name,
+      this.form.value.image
+    );
     this.form.reset();
     this.imageData = '';
+    this.fetchCategories();
     // this.router.navigate(['/', 'category']);
   }
 
   onFileSelect(event: Event) {
-    console.log('something selected');
     const inputFiles = (event.target as HTMLInputElement).files;
     if (null !== inputFiles) {
       const file = inputFiles[0];
-      this.form.patchValue({ image : file});
-      const allowedMimeTypes = ["image/png", "image/jpg", "image/jpeg"];
+      this.form.patchValue({ image: file });
+      const allowedMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
       if (file && allowedMimeTypes.includes(file.type)) {
         const reader = new FileReader();
         reader.onload = () => {
@@ -48,5 +64,4 @@ export class AddCategoryComponent implements OnInit {
       }
     }
   }
-
 }
